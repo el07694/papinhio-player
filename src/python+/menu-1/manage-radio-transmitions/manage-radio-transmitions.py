53958b6d256fc9930429e0520b4942448904d460
Chris Pappas <el07694@gmail.com>
2022-02-07 00:35:46 +0200

    modify of src/python+/main-window/papinhio-player.py (in progress)

--- src/python+/menu-1/manage-radio-transmitions/manage-radio-transmitions.py
+++ src/python+/menu-1/manage-radio-transmitions/manage-radio-transmitions.py
@@ -8,13 +8,13 @@ class Support_Ui_Dialog:
         
         #apply theme
         self.font = QtGui.QFont(self.main_self.default_font, int(self.main_self.default_font_size))
-        self.main_self.edit_radio_stations_window.setStyleSheet("*{font-family:""+self.main_self.default_font+"";font-size:"+self.main_self.default_font_size+"px;color:""+self.main_self.default_font_color+"";}QFrame{border:0px;}QDialog{background:""+self.main_self.default_background_color+""}QPushButton, QComboBox{background:""+self.main_self.default_buttons_background+"";color:""+self.main_self.default_buttons_font_color+""}")
-        self.main_self.edit_radio_stations_window.showMaximized()
-        self.main_self.edit_radio_stations_window.update()
+        self.main_self.manage_radio_connections_window.setStyleSheet("*{font-family:""+self.main_self.default_font+"";font-size:"+self.main_self.default_font_size+"px;color:""+self.main_self.default_font_color+"";}QFrame{border:0px;}QDialog{background:""+self.main_self.default_background_color+""}QPushButton, QComboBox{background:""+self.main_self.default_buttons_background+"";color:""+self.main_self.default_buttons_font_color+""}")
+        self.main_self.manage_radio_connections_window.showMaximized()
+        self.main_self.manage_radio_connections_window.update()
         
-        self.main_self.manage_radio_transmitions_window.closeEvent = lambda event:self.closeEvent(event)
+        self.main_self.manage_radio_connections_window.closeEvent = lambda event:self.closeEvent(event)
         
     def closeEvent(self,event):
-        self.main_self.manage_radio_transmitions_window_is_open = False
+        self.main_self.manage_radio_connections_window_is_open = False
         event.accept()        
         	
--- src/python+/menu-1/sound-clips/export/export.py
+++ src/python+/menu-1/sound-clips/export/export.py
@@ -8,11 +8,11 @@ class Support_Ui_Dialog:
         
         #apply theme
         self.font = QtGui.QFont(self.main_self.default_font, int(self.main_self.default_font_size))
-        self.main_self.edit_radio_stations_window.setStyleSheet("*{font-family:""+self.main_self.default_font+"";font-size:"+self.main_self.default_font_size+"px;color:""+self.main_self.default_font_color+"";}QFrame{border:0px;}QDialog{background:""+self.main_self.default_background_color+""}QPushButton, QComboBox{background:""+self.main_self.default_buttons_background+"";color:""+self.main_self.default_buttons_font_color+""}")
-        self.main_self.edit_radio_stations_window.showMaximized()
-        self.main_self.edit_radio_stations_window.update()
+        self.main_self.export_sound_clips_window.setStyleSheet("*{font-family:""+self.main_self.default_font+"";font-size:"+self.main_self.default_font_size+"px;color:""+self.main_self.default_font_color+"";}QFrame{border:0px;}QDialog{background:""+self.main_self.default_background_color+""}QPushButton, QComboBox{background:""+self.main_self.default_buttons_background+"";color:""+self.main_self.default_buttons_font_color+""}")
+        self.main_self.export_sound_clips_window.showMaximized()
+        self.main_self.export_sound_clips_window.update()
         
-        self.main_self.export_sound_clip_window.closeEvent = lambda event:self.closeEvent(event)
+        self.main_self.export_sound_clips_window.closeEvent = lambda event:self.closeEvent(event)
         
     def closeEvent(self,event):
         self.main_self.export_sound_clip_window_is_open = False
