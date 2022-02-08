206012fc045c239a1d19b22f51efd4d294e40c57
Chris Pappas <el07694@gmail.com>
2022-02-08 10:16:37 +0200

    programm abstract information ready

--- src/python+/menu-4/program-abstract-information/error.py
+++ src/python+/menu-4/program-abstract-information/error.py
@@ -3,7 +3,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Support_Ui_Dialog:
 
-    def __init__(self,main_self):
+    def __init__(self,main_self,error_message):
         self.main_self = main_self
         
         #apply theme
@@ -11,10 +11,38 @@ class Support_Ui_Dialog:
         self.main_self.programm_abstract_information_error_window.setStyleSheet("*{font-family:""+self.main_self.default_font+"";font-size:"+self.main_self.default_font_size+"px;color:""+self.main_self.default_font_color+"";}QFrame{border:0px;}QDialog{background:""+self.main_self.default_background_color+""}QPushButton, QComboBox{background:""+self.main_self.default_buttons_background+"";color:""+self.main_self.default_buttons_font_color+""}")
         self.main_self.programm_abstract_information_error_window.showMaximized()
         self.main_self.programm_abstract_information_error_window.update()
+
+        self.main_self.programm_abstract_information_error_window.hide()
+        self.main_self.programm_abstract_information_error_window.show()
+
+        self.main_self.ui_programm_abstract_information_error_window.details.setPlainText(str(error_message))
+
+        self.main_self.ui_programm_abstract_information_error_window.restart_proccess.clicked.connect(lambda state:self.restart_proccess(state))
+        self.main_self.ui_programm_abstract_information_error_window.ok.clicked.connect(lambda state:self.close_window(state))
+
         
         self.main_self.programm_abstract_information_error_window.closeEvent = lambda event:self.closeEvent(event)
         
+    def restart_proccess(self,state):
+        if self.main_self.programm_abstract_information_window_is_open:
+            self.main_self.programm_abstract_information_window_support_code.close_window(None)
+            self.main_self.programm_abstract_information_window_is_open = False
+
+        self.main_self.programm_abstract_information_error_window.close()
+        self.main_self.open_programm_abstract_information_window(None)
+
+    def close_window(self,state):
+        if self.main_self.programm_abstract_information_window_is_open:
+            self.main_self.programm_abstract_information_window_support_code.close_window(None)
+            self.main_self.programm_abstract_information_window_is_open = False
+
+        self.main_self.programm_abstract_information_error_window.close()
+        
     def closeEvent(self,event):
+        if self.main_self.programm_abstract_information_window_is_open:
+            self.main_self.programm_abstract_information_window_support_code.close_window(None)
+            self.main_self.programm_abstract_information_window_is_open = False
+
+
         self.main_self.programm_abstract_information_error_window_is_open = False
-        event.accept()        
-        	
+        event.accept()        	
--- src/python+/menu-4/program-abstract-information/program-abstract-information.py
+++ src/python+/menu-4/program-abstract-information/program-abstract-information.py
@@ -6,31 +6,48 @@ import traceback
 class Support_Ui_Dialog:
 
     def __init__(self,main_self):
-        self.main_self = main_self
-        
-        #apply theme
-        self.font = QtGui.QFont(self.main_self.default_font, int(self.main_self.default_font_size))
-        self.main_self.programm_abstract_information_window.setStyleSheet("*{font-family:""+self.main_self.default_font+"";font-size:"+self.main_self.default_font_size+"px;color:""+self.main_self.default_font_color+"";}QFrame{border:0px;}QDialog{background:""+self.main_self.default_background_color+""}QPushButton, QComboBox{background:""+self.main_self.default_buttons_background+"";color:""+self.main_self.default_buttons_font_color+""}")
-        self.main_self.programm_abstract_information_window.showMaximized()
-        self.main_self.programm_abstract_information_window.update()
+        try:
+            self.main_self = main_self
+            
+            #apply theme
+            self.font = QtGui.QFont(self.main_self.default_font, int(self.main_self.default_font_size))
+            self.main_self.programm_abstract_information_window.setStyleSheet("*{font-family:""+self.main_self.default_font+"";font-size:"+self.main_self.default_font_size+"px;color:""+self.main_self.default_font_color+"";}QFrame{border:0px;}QDialog{background:""+self.main_self.default_background_color+""}QPushButton, QComboBox{background:""+self.main_self.default_buttons_background+"";color:""+self.main_self.default_buttons_font_color+""}")
+            self.main_self.programm_abstract_information_window.showMaximized()
+            self.main_self.programm_abstract_information_window.update()
 
-        self.main_self.programm_abstract_information_window.show()
-        self.main_self.programm_abstract_information_window.hide()
+            self.main_self.programm_abstract_information_window.show()
+            self.main_self.programm_abstract_information_window.hide()
 
-        self.main_self.ui_programm_abstract_information_window.ok.clicked.connect(lambda state:self.close_window(state))
-        self.main_self.ui_programm_abstract_information_window.open_manual.clicked.connect(lambda state:self.open_manual(state))
-        
-        self.main_self.programm_abstract_information_window.closeEvent = lambda event:self.closeEvent(event)
+            self.main_self.ui_programm_abstract_information_window.ok.clicked.connect(lambda state:self.close_window(state))
+            self.main_self.ui_programm_abstract_information_window.open_manual.clicked.connect(lambda state:self.open_manual(state))
+            
+            self.main_self.programm_abstract_information_window.closeEvent = lambda event:self.closeEvent(event)
+        except Exception as e:
+            error_message = str(traceback.format_exc())
+            self.main_self.open_programm_abstract_information_error_window(error_message)
   
     def close_window(self,state):
-        self.main_self.programm_abstract_information_window.close()
+        try:
+            self.main_self.programm_abstract_information_window.close()
+        except Exception as e:
+            error_message = str(traceback.format_exc())
+            self.main_self.open_programm_abstract_information_error_window(error_message)
         
     def open_manual(self,state):
-        manual_pdf_path = os.path.abspath("")
-        subprocess.Popen('explorer /select,"'+manual_pdf_path+'"', shell=False)
+        try:
+            manual_pdf_path = os.path.abspath("")
+            subprocess.Popen('explorer /select,"'+manual_pdf_path+'"', shell=False)
+        except Exception as e:
+            error_message = str(traceback.format_exc())
+            self.main_self.open_programm_abstract_information_error_window(error_message)
         self.close_window(None)
 
     def closeEvent(self,event):
-        self.main_self.programm_abstract_information_window_is_open = False
-        event.accept()        
+        try:
+            self.main_self.programm_abstract_information_window_is_open = False
+            event.accept()        
+        except Exception as e:
+            error_message = str(traceback.format_exc())
+            self.main_self.open_programm_abstract_information_error_window(error_message)
+        
         	
